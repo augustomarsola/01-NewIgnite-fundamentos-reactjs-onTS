f024b8ddf21bd72ffa05fb757841b5724605ccdc
Augusto Marsola <augusto.marsola@live.com>
2022-06-10 08:39:05 -0300

    Finished the translate to TypeScript

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,15 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.scss";
 
-export function Avatar({ hasborder = true, src }) {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
+  hasborder?: boolean;
+}
+
+export function Avatar({ hasborder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasborder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
+      src={props.src}
     />
   );
 }
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.scss";
 
-export function Comment({ content, onDeleteComment }) {
+interface CommentsProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentsProps) {
   const [clap, setClap] = useState(0);
 
   function handleDeleteComment() {
